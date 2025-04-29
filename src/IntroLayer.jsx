ac3a12fb6eb172719fffe396e816dd54ef97d55c
Aichaz <el.mardizarrouk@gmail.com>
2025-04-29 17:08:25 +0100

    up

--- src/IntroLayer.jsx
+++ src/IntroLayer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./IntroLayer.css"; // Use this if you're keeping the original CSS
-
+import { motion } from 'framer-motion';
 const IntroLayer = () => {
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -21,14 +21,39 @@ const IntroLayer = () => {
   return (
     <div className="layer">
       <img src="images/avv.png" alt="plane" className="plane" />
-      <img src="images/morocco-map.png" alt="morocco map" className="mr-map" />
+      {/* <img src="images/morocco-map.png" alt="morocco map" className="mr-map" />
       <div className="intro-message">
         <h1>Welcome to KM0</h1>
         <p>Travel with us and discover Morocco</p>
         <button id="travelBtn" onClick={handleTravel}>
           Travel
         </button>
-      </div>
+      </div> */}
+      <motion.img
+        src="images/morocco-map.png"
+        alt="morocco map"
+        className="mr-map"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      />
+      <motion.div
+        className="intro-message"
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1}}
+        transition={{ duration: 3}}
+      >
+        <h1>Embark on a Flavorful Adventure at <br /> KMO </h1>
+        <p>Through Morocco to the Heart of Africa</p>
+        <motion.button
+          id="travelBtn"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          onClick={handleTravel}
+        >
+         <span>Travel with us</span> 
+        </motion.button>
+      </motion.div>
     </div>
   );
 };
