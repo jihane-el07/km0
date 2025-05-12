a2fc259af8bb9c8bef822c605491f894e00ae8af
Aichaz <el.mardizarrouk@gmail.com>
2025-05-12 13:55:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import IntroLayer from './pages/intro/IntroLayer';
 import Home from './pages/Home/Home';
 import Nav from './pages/Nav/Nav';
 import Events from './pages/Events/Events';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.location.pathname !== '/') {
+      navigate('/');
+    }
+  }, []);
   return (
     <div className="App">
+      <ScrollToTop />
       <IntroLayer/>
       <Nav/>
       <Routes>
